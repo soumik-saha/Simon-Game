bb1b4c2e00b1727a6c3745fb1833a1ddd39cda12
Soumik Saha <sahasoumik1573@gmail.com>
2023-03-10 11:02:02 +0530

    Updated game.js

--- game.js
+++ game.js
@@ -1,12 +1,48 @@
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColor);
+  level++;
+  $("#level-title").text("Level " + level);
 
-    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).faceIn(100);
-    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
-    audio.play();
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
